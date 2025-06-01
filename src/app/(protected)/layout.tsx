dcc1d03540e643a73e2192e7594f959bd5d03e6a
Everton Luis <evertonluis.pro@outlook.com>
2025-06-01 16:30:32 -0300

    feat: add sidebar

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,17 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+import { AppSidebar } from "./_components/app-sidebar";
+
+const ProtectedPage = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default ProtectedPage;
