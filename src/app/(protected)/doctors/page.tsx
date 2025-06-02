a2c9c1f3a5d8b56f33d0e235bca310cef183141b
Everton Luis <evertonluis.pro@outlook.com>
2025-06-01 23:38:27 -0300

    feat: ensure user has clinic on doctor page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,26 +1,53 @@
-import { Button } from "@/components/ui/button";
-import { PageActions, PageContainer, PageContent, PageDescription, PageHeader, PageHeaderContent, PageTitle } from "@/components/ui/page-container";
+import { eq } from "drizzle-orm";
 import { Plus } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const DoctorsPage = () => {
-    return (
-        <PageContainer>
-          <PageHeader>
-            <PageHeaderContent>
-              <PageTitle>Médicos</PageTitle>
-              <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
-            </PageHeaderContent>
-            <PageActions>
-              <Button>
-                <Plus/>
-                Adicionar Médico</Button>
-            </PageActions>
-          </PageHeader>
-        <PageContent>
-            <h1>Médicos</h1>
-        </PageContent>
-        </PageContainer>
-      );
-    };
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+import { doctorsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+const DoctorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+  const doctors = await db.query.doctorsTable.findMany({
+    where: eq(doctorsTable.clinicId, session.user.clinic.id),
+  });
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar Médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>Médicos</h1>
+      </PageContent>
+    </PageContainer>
+  );
+};
 
 export default DoctorsPage;
